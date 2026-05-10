f5118ebaff5387dea0418a826a4f6b82f80ee706
Mohamed Ahmed <muhammetahmed12@gmail.com>
2026-05-10 19:34:10 +0300

    done welcome section, start continue wtching

--- app/[locale]/student/page.tsx
+++ app/[locale]/student/page.tsx
@@ -1,16 +1,41 @@
+import ContinueWatchingSection from "@/src/components/student/home/ContinueWatchingSection";
 import WelcomeSection from "@/src/components/student/home/WelcomeSection";
-import { getStudentCourses, getStudentData, getStudentLiveSessions, getStudentProgression } from "@/src/services/student/user.service";
+import {
+  getStudentCourses,
+  getStudentData,
+  getStudentLiveSessions,
+  getStudentProgression,
+} from "@/src/services/student/user.service";
 
+export default async function StudentPage() {
+  const {
+    data: {
+      data: { attributes: student },
+    },
+  } = await getStudentData();
+  const {
+    data: { data: progress },
+  } = await getStudentProgression();
+  const {
+    data: { data: courses },
+  } = await getStudentCourses();
+  const {
+    data: { data: liveSessions },
+  } = await getStudentLiveSessions();
 
-export default async function  StudentPage () {
- const {data:{data:{attributes:student}}} =await getStudentData();
-const {data:{data: progress}}= await  getStudentProgression();
-const {data:{data:courses}}= await getStudentCourses();
-const {data:{data:liveSessions}}= await getStudentLiveSessions();
+  const coursesCount = courses?.length;
+  const progressCount = progress?.length;
+  const liveSessionsCount = liveSessions?.length;
 
-
-
-  return <div >
-<WelcomeSection courses={courses} progress={progress} student={student} liveSessions={liveSessions} />
-  </div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <WelcomeSection
+        coursesCount={coursesCount}
+        progressCount={progressCount}
+        student={student}
+        liveSessionsCount={liveSessionsCount}
+      />
+      <ContinueWatchingSection liveSessions={liveSessions} />
+    </div>
+  );
 }
--- src/components/student/home/WelcomeSection.tsx
+++ src/components/student/home/WelcomeSection.tsx
@@ -2,14 +2,36 @@ import { ICourse } from '@/src/interfaces/courses.interface'
 import { ILiveSessions } from '@/src/interfaces/livesessions.interface'
 import { IProgress } from '@/src/interfaces/progress.interface'
 import { IStudent } from '@/src/interfaces/student.interface'
+import { BookOpen, FileText, Video } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 
-const WelcomeSection = ({student, progress, courses, liveSessions}: {student: IStudent, progress: IProgress, courses: ICourse, liveSessions: ILiveSessions}) => {
+
+
+const WelcomeSection = ({coursesCount, progressCount, student, liveSessionsCount}: {student: IStudent, progressCount: number, coursesCount: number, liveSessionsCount: number}) => {
    const t = useTranslations("student.home.welcome");
+   const tC = useTranslations("universities");
+  
   return (
-    <div className='p-8 bg-gradient-to-r from-[#4F46E5] to-[#7C3AED] rounded-2xl text-white'>
-      <h2>{t('title')}</h2>
-    </div>
+    <section className='p-8 bg-gradient-to-r from-[#4F46E5] to-[#7C3AED] rounded-2xl text-white flex flex-col gap-4'>
+      <h2 className='text-3xl lg:text-5xl font-bold'>{`${t('title')} ${student.first_name} 👋` } </h2>
+      <p >{student?.university?.data?.attributes.name || tC("notFound")}</p>
+      <div className=" flex flex-wrap items-center gap-6 text-sm text-white/90">
+  <div className="flex items-center gap-2">
+    <BookOpen size={16} />
+    <span>{coursesCount} Active Courses</span>
+  </div>
+
+  <div className="flex items-center gap-2">
+    <Video size={16} />
+    <span>{liveSessionsCount} Live Today</span>
+  </div>
+
+  <div className="flex items-center gap-2">
+    <FileText size={16} />
+    <span>{progressCount} Upcoming Exams</span>
+  </div>
+</div>
+    </section>
   )
 }
 
